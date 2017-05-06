2b07a8002181617cc8d0f9442825838f1548f6a7
STARasGAMES <doterpoter@gmail.com>
2017-05-07 00:47:33 +0300

    06-05-2017

---
+++ Assets/ARQuestCreator/Scripts/Inventory/Item.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+namespace ARQuestCreator
+{
+    
+    [RequireComponent(typeof(BoxCollider))]
+    public class Item : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+    {
+        public string name = "defaultItemName";
+        [TextArea(3, 20)]
+        public string description = "default description";
+        public bool pickable = true;
+        public bool canBeViewed = true;
+        public bool rotatable = true;
+        public bool scalable = true;
+
+        private Collider _collider;
+
+#region Unity MonoBehaviur Events
+        private void OnEnable()
+        {
+            SetChildActive(true);
+            _collider = GetComponent<Collider>();
+            _collider.enabled = pickable;
+        }
+
+        private void OnDisable()
+        {
+            SetChildActive(false);
+        }
+#endregion //Unity MonoBehaviur Events
+
+        private void SetChildActive(bool value)
+        {
+            foreach (Transform t in transform)
+            {
+                t.gameObject.SetActive(value);
+            }
+        }
+
+#region IPointerHandlerImplementation
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+           // throw new NotImplementedException();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+          //  throw new NotImplementedException();
+        }
+    }
+#endregion // IPointerHandlerImplementation
+
+   
+}
+
+
---
+++ Assets/ARQuestCreator/Scripts/UI/Inventory/UIItemController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIItemController : MonoBehaviour {
+
+    [SerializeField] Text _itemNameText;
+    [SerializeField] Image _itemImage;
+
+    public void ShowItem()
+    {
+        gameObject.SetActive(true);
+    }
+
+}
